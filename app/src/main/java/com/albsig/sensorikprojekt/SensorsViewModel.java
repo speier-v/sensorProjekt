8a65b89953ca72cd13a1fc430a1d0ad276ec5ba6
speier-v <127931979+speier-v@users.noreply.github.com>
2024-10-07 11:21:06 +0200

    Implement text output

--- app/src/main/java/com/albsig/sensorikprojekt/SensorsViewModel.java
+++ app/src/main/java/com/albsig/sensorikprojekt/SensorsViewModel.java
@@ -4,15 +4,25 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
 public class SensorsViewModel extends ViewModel {
     private final MutableLiveData<Integer> selectedText = new MutableLiveData<>();
+    private final MutableLiveData<List<String>> textData = new MutableLiveData<>();
 
     public void setText(Integer text) {
         selectedText.setValue(text);
+    }
+
+    public void setTextData(List<String> text) {
+        textData.postValue(text);
     }
 
     public LiveData<Integer> getText() {
         return selectedText;
     }
 
+    public LiveData<List<String>> getTextData() {
+        return textData;
+    }
 }
--- app/src/main/java/com/albsig/sensorikprojekt/TextOutputFragment.java
+++ app/src/main/java/com/albsig/sensorikprojekt/TextOutputFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,17 +14,27 @@ import com.albsig.sensorikprojekt.databinding.FragmentTextOutputBinding;
 
 public class TextOutputFragment extends Fragment {
 
-private FragmentTextOutputBinding binding;
-private SensorsViewModel sensorsViewModel;
+    private FragmentTextOutputBinding binding;
+    private SensorsViewModel sensorsViewModel;
+    private TextView textView;
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
       binding = FragmentTextOutputBinding.inflate(inflater, container, false);
+      textView = binding.getRoot().findViewById(R.id.textOutputTV);
       sensorsViewModel = new ViewModelProvider(requireActivity()).get(SensorsViewModel.class);
+
+      sensorsViewModel.getTextData().observe(getViewLifecycleOwner(), textList -> {
+          StringBuilder formattedText = new StringBuilder();
+          for (String text : textList) {
+              formattedText.append(text).append("\n");
+          }
+          textView.setText(formattedText.toString());
+      });
+
       return binding.getRoot();
 
     }
@@ -33,7 +44,7 @@ private SensorsViewModel sensorsViewModel;
 
     }
 
-@Override
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
